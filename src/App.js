8e0b5fbc6d914fc88eb496df9cbf5a5d9abe74a8
kukkonenjoni <joni.kukkonen1@gmail.com>
2021-08-15 22:37:10 +0300

    Memory Game initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import RenderChamps from './components/RenderChamps';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+      super();
+      this.state = {
+        champions: "",
+        curr_field: "",
+        gamestate: true,
+        count: 0,
+        multiplier: 4
+      }
+      this.randomChamps = this.randomChamps.bind(this)
+      this.updateField = this.updateField.bind(this)
+  }
+  populateChampion(data) {
+    let empty = []
+    for (let champion in data) {
+      let champ = [data[champion].id, data[champion].image.full]
+      empty.push(champ)
+    }
+    this.setState({champions: empty})
+  }
+  componentDidMount() {
+    fetch("http://ddragon.leagueoflegends.com/cdn/9.18.1/data/en_US/champion.json")
+    .then(response => response.json())
+    .then(data => data.data)
+    .then(data => this.populateChampion(data))
+  }
+  randomChamps() {
+    let empty = []
+    for (let i = 1; i <= this.state.multiplier; i++) {
+      let random = Math.floor(Math.random() * 145);
+      let new_arr = this.state.champions[random].slice(0,2)
+      empty.push(new_arr)
+    }
+    this.setState({curr_field: empty})
+  }
+  updateField(e, index, all_champs) {
+    let new_arr = all_champs.slice()
+    if (new_arr[index][2] == true) {
+      this.setState({gamestate: false})
+    }
+    else {
+      new_arr[index][2] = true
+      this.setState({curr_field: new_arr})
+      this.setState(prevState => ({count: prevState.count +1}))
+    }
+    if (this.state.count > this.state.multiplier - 2) {
+      this.setState({curr_field: ""})
+      this.setState({count: 0})
+      this.setState(prevState => ({multiplier: prevState.multiplier + 1}))
+      console.log(this.state.curr_field)
+    }
+    console.log(this.state.curr_field)
+  }
+  render() {
+    let display;
+    if (this.state.gamestate == false) {
+      display = <h1>You Lost</h1>
+    }
+    else if (this.state.curr_field === "" && this.state.champions.length === 145) {
+      this.randomChamps()
+    }
+    else if (this.state.champions.length === 145 && this.state.curr_field !== "") {
+      display = <RenderChamps value={this.state.curr_field} btn={this.updateField}/>
+    }
+    else {
+      display = <h1>Loading.....</h1>
+    }
+    return(
+      <div>
+        <h1>Hello World!</h1>
+        {display}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/RenderChamps.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+function RenderChamps(props) {
+    
+    const [champions, setChampions] = useState(props.value)
+    const shuffle = (champions) => {
+        let shuffled = champions.sort((a,b) => 0.5-Math.random())
+        return shuffled
+    }
+    let images = shuffle(champions)
+    images = images.map((champion, index) => {
+        let src = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + champion[0] + "_0.jpg"
+        return (
+            <div className="picture" key={index}>
+                <img src = {src} alt={champion[0]} width="300px" height="250px" onClick={(e) => props.btn(e, index, champions)} />
+                <h1 className="champion-name">{champion[0]}</h1>
+            </div>
+        )
+    })
+    return(
+        <div className="pictures">
+            {images}
+        </div>
+    )
+}
+
+export default RenderChamps
